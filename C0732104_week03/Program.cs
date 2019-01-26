1e213deec78fa16211b99de5e7fb2f16189a002d
visualstudio <visualstudio@122-PC05>
2019-01-25 21:08:11 -0500

    commit at 9:08

--- C0732104_week03/Program.cs
+++ C0732104_week03/Program.cs
@@ -12,14 +12,42 @@ namespace C0732104_week03
         {
         }
     }
-
-    class Village
+    class Countrysider
     {
-        Village PreviousVillage;
-        Village NextVillage;
 
-        String VillageName;
-        bool isAstrildeHere = false;
+            Village Alst;
+            Village Maeland;
+            Village Schenig;
+
+        public void InitializeMap()
+        {
+
+
+             Alst = new Village();
+            Alst.VillageName = "Alst";
+             Maeland = new Village();
+            Maeland.VillageName = "Maeland";
+            Maeland.isAstrildeHere = true;
+             Schenig = new Village();
+            Schenig.VillageName = "Schenig";
+
+            Alst.NextVillage = Maeland;
+            Maeland.NextVillage = Schenig;
+            Schenig.NextVillage = Alst;
+        }
+
+        public void WalkAround()
+        {
+            Village InitializeVillage = this.Maeland;
+        }
+    }
+
+ class Village
+    {
+        public Village PreviousVillage;
+        public Village NextVillage;
+        public String VillageName;
+        public bool isAstrildeHere = false;
         public   Village() { }
 
 
